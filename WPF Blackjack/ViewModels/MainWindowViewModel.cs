0d8d8bcd6d20a0adde6e64e9f2b04e16aec3f346
YongCho <yongjec@gmail.com>
2016-03-17 16:58:43 -0400

    Add scores to view

--- WPF Blackjack/ViewModels/MainWindowViewModel.cs
+++ WPF Blackjack/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Windows.Input;
 using WPF_Blackjack.Models;
@@ -14,6 +15,20 @@ namespace WPF_Blackjack.ViewModels
         public ObservableCollection<Card> PlayerCards { get; set; }
         public ObservableCollection<Card> DealerCards { get; set; }
 
+        private string playerHandValue = string.Empty;
+        public string PlayerHandValue
+        {
+            get { return this.playerHandValue; }
+            set { SetProperty(ref this.playerHandValue, value); }
+        }
+
+        private string dealerHandValue = string.Empty;
+        public string DealerHandValue
+        {
+            get { return this.dealerHandValue; }
+            set { SetProperty(ref this.dealerHandValue, value); }
+        }
+
         public ICommand DealCommand { get; set; }
         public ICommand HitCommand { get; set; }
         public ICommand StandCommand { get; set; }
@@ -23,11 +38,30 @@ namespace WPF_Blackjack.ViewModels
         {
             PlayerCards = new ObservableCollection<Card>();
             DealerCards = new ObservableCollection<Card>();
+            PlayerCards.CollectionChanged += HandlePlayerHandChange;
+            DealerCards.CollectionChanged += HandleDealerHandChange;
 
             DealCommand = new DelegateCommand(DoDeal);
             HitCommand = new DelegateCommand(DoHit);
             StandCommand = new DelegateCommand(DoStand);
             SplitCommand = new DelegateCommand(DoSplit);
+        }
+
+        private void HandlePlayerHandChange(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            PlayerHandValue = Convert.ToString(CalculateValue(PlayerCards));
+        }
+
+        private void HandleDealerHandChange(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            DealerHandValue = Convert.ToString(CalculateValue(DealerCards));
+        }
+
+        private int CalculateValue(ObservableCollection<Card> hand)
+        {
+            int value = 0;
+
+            return value;
         }
 
         private void DoDeal()
