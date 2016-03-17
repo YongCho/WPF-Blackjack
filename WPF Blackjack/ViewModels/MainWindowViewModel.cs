c06f6b43cfb7dc3dd8bb0f1e9e131a1c619bf638
YongCho <yongjec@gmail.com>
2016-03-17 16:15:36 -0400

    Add dealer cards

--- WPF Blackjack/ViewModels/MainWindowViewModel.cs
+++ WPF Blackjack/ViewModels/MainWindowViewModel.cs
@@ -12,18 +12,17 @@ namespace WPF_Blackjack.ViewModels
     public class MainWindowViewModel : BindableBase
     {
         public ObservableCollection<Card> PlayerCards { get; set; }
-
+        public ObservableCollection<Card> DealerCards { get; set; }
 
         public ICommand DealCommand { get; set; }
         public ICommand HitCommand { get; set; }
         public ICommand StandCommand { get; set; }
         public ICommand SplitCommand { get; set; }
 
-
-
         public MainWindowViewModel()
         {
             PlayerCards = new ObservableCollection<Card>();
+            DealerCards = new ObservableCollection<Card>();
 
             DealCommand = new DelegateCommand(DoDeal);
             HitCommand = new DelegateCommand(DoHit);
