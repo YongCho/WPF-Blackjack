2cc522ba35b500f1d0af039ebecdf3175bbac47f
YongCho <yongjec@gmail.com>
2016-03-17 10:26:41 -0400

    Add MainWindow ViewModel

---
+++ WPF Blackjack/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using WPF_Blackjack.Models;
+
+namespace WPF_Blackjack.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
