bf38e35001f251f9dd593cf1c5f147250cfd0852
YongCho <yongjec@gmail.com>
2016-03-17 12:37:23 -0400

    Add deal button

--- WPF Blackjack/ViewModels/MainWindowViewModel.cs
+++ WPF Blackjack/ViewModels/MainWindowViewModel.cs
@@ -9,15 +9,22 @@ namespace WPF_Blackjack.ViewModels
 {
     public class MainWindowViewModel
     {
+        public ICommand DealCommand { get; set; }
         public ICommand HitCommand { get; set; }
         public ICommand StandCommand { get; set; }
         public ICommand SplitCommand { get; set; }
 
         public MainWindowViewModel()
         {
+            DealCommand = new DelegateCommand(DoDeal);
             HitCommand = new DelegateCommand(DoHit);
             StandCommand = new DelegateCommand(DoStand);
             SplitCommand = new DelegateCommand(DoSplit);
+        }
+
+        private void DoDeal()
+        {
+            Trace.WriteLine("Deal");
         }
 
         private void DoHit()
