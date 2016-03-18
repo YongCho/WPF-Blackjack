3e5bc1e120b0bb4b0d2f9013e5f93e3e5f1e2d43
YongCho <yongjec@gmail.com>
2016-03-18 07:31:53 -0400

    Implement calculating hand value

--- Blackjack/Models/BlackJackGame.cs
+++ Blackjack/Models/BlackJackGame.cs
@@ -31,9 +31,38 @@ namespace BlackJack.Models
 
         public int CalculateValue(List<Card> hand)
         {
-            int value = 0;
+            int handValue = 0;
+            int aceCount = 0;
 
-            return value;
+            foreach (Card card in hand)
+            {
+                // Face-down card doesn't add to the hand's value.
+                if (card.IsFaceDown)
+                {
+                    continue;
+                }
+
+                if (card.FaceNumber <= 10)
+                {
+                    handValue += card.FaceNumber;
+                    if (card.FaceNumber == 1)
+                    {
+                        ++aceCount;
+                    }
+                }
+                else
+                {
+                    handValue += 10;
+                }
+            }
+
+            // Raise one ace to 11 if it will not bust the hand.
+            if (handValue <= 11 && aceCount > 0)
+            {
+                handValue += 10;
+            }
+
+            return handValue;
         }
 
         public Card DealCard()
